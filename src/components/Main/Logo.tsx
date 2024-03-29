c913a308add07df3fcab708929c5839bd3b7b32c
Ivan <karez79@yandex.ru>
2024-03-29 21:04:49 +0100

    add: main

---
+++ src/components/Main/Logo.tsx
@@ -0,0 +1,5 @@
+import LogoImage from "../../assets/logoVink.svg";
+
+const Logo = () => <img src={LogoImage} alt="Логотип" />;
+
+export default Logo;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,20 @@
-import Modal from '../Modal/Modal';
-import './Main.css';
+import Logo from "./Logo";
+import CompanyDescription from "./CompanyDescription";
+import ChatButton from "./ChatButton";
+import Modal from "../Modal/Modal";
+import "./Main.css";
 
-export default function Main() {
-  return (
-    <div className='main'>
-      {/* Впиши сюда твой компонент, например Page */}
+const Main = () => (
+  <div className="main">
+    <div className="first-section">
+      <Logo />
+      <CompanyDescription />
+    </div>
+    <div className="second-section">
+      <ChatButton />
       <Modal />
     </div>
-  );
-}
+  </div>
+);
+
+export default Main;
