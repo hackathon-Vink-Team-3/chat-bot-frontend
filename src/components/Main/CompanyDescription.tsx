5bd1fefdb6b02ba7ca5319d72fab2909f13888f1
Nickolai <lindeman072@mail.ru>
2024-03-31 01:03:15 +0400

    fix: main landing

--- src/components/Main/CompanyDescription.tsx
+++ src/components/Main/CompanyDescription.tsx
@@ -1,8 +1,12 @@
+import './Main.css';
+
 const COMPANY_DESCRIPTION =
-  "Компания Винк — крупнейший поставщик материалов и оборудования для рынка визуальных офлайн-коммуникаций";
+  'Компания Винк — крупнейший поставщик материалов и оборудования для рынка визуальных офлайн-коммуникаций';
 
 const CompanyDescription = () => (
-  <p className="subtitle">{COMPANY_DESCRIPTION}</p>
+  <p className='subtitle' style={{ width: '418px' }}>
+    {COMPANY_DESCRIPTION}
+  </p>
 );
 
 export default CompanyDescription;
