5d0d18a543fea4f1cc30042b8a02a493a034962a
Nickolai <lindeman072@mail.ru>
2024-04-04 16:33:58 +0400

    add: post

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,153 @@
 import Main from '../Main/Main';
 import './App.css';
+import * as Api from './../../utils/utils';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [chat, setChat] = useState('');
+    const [dialog, setDialog] = useState('');
+
+  console.log('dialog: ', dialog);
+
+  // const postChat = () => {
+  //   // Получаем данные чата из локального хранилища
+  //   const storedChat = localStorage.getItem('chat');
+  //   if (storedChat) {
+  //     setChat(storedChat);
+  //   } else {
+  //     // Если данных чата нет в локальном хранилище, делаем POST запрос на сервер
+  //     Api.postChat()
+  //       .then((data) => {
+  //         setChat(data.id);
+  //         console.log('dataGetChats: ', data.id);
+  //         // Сохраняем данные чата в локальное хранилище
+  //         localStorage.setItem('chat', data.id);
+  //       })
+  //       .catch(error => console.error('Error:', error));
+  //   }
+  // }
+
+  const postChat = () => {
+    // Получаем данные чата из локального хранилища
+    const storedChat = localStorage.getItem('chat');
+    if (storedChat) {
+      setChat(storedChat);
+    } else {
+      // Если данных чата нет в локальном хранилище, делаем POST запрос на сервер
+      Api.postChat()
+        .then((data) => {
+          setChat(data.id);
+          console.log('dataGetChats: ', data.id);
+          // Сохраняем данные чата в локальное хранилище
+          localStorage.setItem('chat', data.id);
+        })
+        .catch(error => console.error('Error:', error));
+    }
+  }
+
+  useEffect(() => {
+    postChat(); // Вызываем функцию postChat для получения данных чата
+    if (chat) {
+      Api.getDialog(chat)
+        .then((data) => {
+          setDialog(data);
+          console.log('data: ', data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [chat]);
+
   return (
     <div className='body'>
-     <Main />
+      <Main />
     </div>
   );
 }
+
+
+
+// useEffect(() => {
+//   Api.postChat()
+//     .then((data) => {
+//       postChats(data);
+//       console.log('chat_id: ', data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }, []);
+
+//   useEffect(() => {
+//     Api.postChat().then((id) => {
+//       console.log('chat_uuid: ', id);
+//       Api.getChats(id)
+//         .then((data) => {
+//           postChats(data);
+//           console.log('dataMess: ', data);
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//         });
+//     });
+//   }, []);
+
+//   return (
+//     <div className='body'>
+//       <Main />
+//     </div>
+//   );
+// }
+
+
+//   useEffect(() => {
+//     Api.getChats()
+//       .then((data) => {
+//         setChats(data);
+//         console.log('data: ', data);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   }, []);
+
+//   const postChat = () => {
+//     Api.postChat()
+//       .then(data => {
+//         console.log('data:', data);
+//       })
+//       .catch(error => console.error('Error:', error));
+//   };
+
+//   return (
+//     <div className='body'>
+//      <Main postChat={postChat}/>
+//     </div>
+//   );
+// }
+
+
+
+// useEffect(() => {
+//   Api.getChats()
+//     .then((data) => {
+//       setChats(data);
+//       console.log('dataGetChats: ', data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }, []);
+
+// useEffect(() => {
+//   Api.postChat()
+//     .then(chatId => {
+//       console.log('chatId:', chatId);
+//       return Api.getChats(chatId);
+//     })
+//     .then(info => {
+//       console.log('info:', info);
+//     })
+//     .catch(error => console.error('Error:', error));
+// },  []);
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,15 +3,32 @@ import { useState } from 'react';
 import Layout from './Layout/Layout';
 import Modal from '../Modal/Modal';
 
-export default function Main() {
+// interface MainApp {
+//   postChat: () => void;
+// }
+
+export default function Main(
+  // { postChat }: MainApp
+  ) {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const handleOpenChat = () => {
+    // postChat();
     setIsPopupOpen(true);
   };
 
   const handleCloseChat = () => {
     setIsPopupOpen(false);
   };
+
+  // const [openChats, setOpenChats] = useState(null);
+  // useEffect(() => {
+  //   openChats;
+  // }, []);
+  // console.log('openChats ', openChats);
+
+  // const postChat = () => {
+  //   Api.postChat();
+  // };
 
   return (
     <div className='main'>
