60d092ba3647d77c7049d702c468769c45547fab
Nickolai <lindeman072@mail.ru>
2024-04-09 03:20:22 +0400

    fix: before PR9

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
