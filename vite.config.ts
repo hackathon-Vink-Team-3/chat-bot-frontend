ff8b75239a0ea50264a08983dff86f2fdba5a66b
Nickolai <lindeman072@mail.ru>
2024-04-09 04:10:52 +0400

    fix: before PR11

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import svgr from 'vite-plugin-svgr'
+
+// export default defineConfig({
+//   base: "/",
+//   plugins: [
+//     react(),
+//     svgr({svgrOptions: {
+//         }
+//       })
+//     ],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
@@ -9,5 +23,21 @@ export default defineConfig({
     svgr({svgrOptions: {
         }
       })
-    ]
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: '[name]-[hash].js',
+          chunkFileNames: '[name]-[hash].js',
+          assetFileNames: '[name]-[hash].[ext]'
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        // Поддержка псевдонимов для импортов
+      },
+      extensions: ['.js', '.jsx'], // Устанавливаем MIME-тип для JavaScript файлов
+      mainFields: ['module', 'main'] // Определяем основные поля для разрешения модулей
+    }
 })
