e69cb493009dbd4ab46531655f535a2138e9ae93
Nickolai <lindeman072@mail.ru>
2024-04-09 04:18:17 +0400

    fix: before PR12

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import svgr from 'vite-plugin-svgr'
-
-// export default defineConfig({
-//   base: "/",
-//   plugins: [
-//     react(),
-//     svgr({svgrOptions: {
-//         }
-//       })
-//     ],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
@@ -24,20 +10,5 @@ export default defineConfig({
         }
       })
     ],
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: '[name]-[hash].js',
-          chunkFileNames: '[name]-[hash].js',
-          assetFileNames: '[name]-[hash].[ext]'
-        }
-      }
-    },
-    resolve: {
-      alias: {
-        // Поддержка псевдонимов для импортов
-      },
-      extensions: ['.js', '.jsx'], // Устанавливаем MIME-тип для JavaScript файлов
-      mainFields: ['module', 'main'] // Определяем основные поля для разрешения модулей
-    }
 })
+
